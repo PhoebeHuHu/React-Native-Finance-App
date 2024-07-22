7151c7ce2d7b8059acd3d286636135f3cef269f3
peike-hu <phoebehu233@gmail.com>
2024-07-22 21:29:55 +1000

    set up app, add style of nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,68 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Tabs } from 'expo-router'
+import { AntDesign, FontAwesome, SimpleLineIcons } from '@expo/vector-icons'
+import Colors from '@/constants/Colors'
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+export default function Layout() {
+    return (
+        <Tabs screenOptions={{
+            tabBarStyle: {
+                backgroundColor: Colors.grey,
+                position: 'absolute',
+                bottom: 40,
+                justifyContent: 'center',
+                alignSelf: 'center',
+                height: 63,
+                marginHorizontal: 120,
+                paddingHorizontal: 10,
+                paddingVertical: 8,
+                paddingBottom: 8,
+                borderRadius: 40,
+                borderWidth: 1,
+                borderColor: '#333',
+                //for bottom tab, there is already have a border top, so we have to specifically set the style of border top
+                borderTopWidth: 1,
+                borderTopColor: '#333'
+            },
+            tabBarShowLabel: false,
+            tabBarInactiveTintColor: '#999',
+            tabBarActiveTintColor: Colors.white
+        }}>
+            <Tabs.Screen name='index' options={{
+                tabBarIcon: ({ color, size, focused }) => (
+                    <View style={{
+                        padding: 12,
+                        borderRadius: 30,
+                        backgroundColor: focused ? Colors.tintColor : Colors.grey
+                    }}>
+                        <SimpleLineIcons name='pie-chart' size={18} color={color} />
+                    </View>
+                )
+            }} />
+            <Tabs.Screen name='transactions' options={{
+                tabBarIcon: ({ color, size, focused }) => (
+                    <View style={{
+                        padding: 12,
+                        borderRadius: 30,
+                        backgroundColor: focused ? Colors.tintColor : Colors.grey
+                    }}>
+                        <AntDesign name='swap' size={18} color={color} />
+                    </View>
+                )
+            }} />
+            <Tabs.Screen name='profile' options={{
+                tabBarIcon: ({ color, size, focused }) => (
+                    <View style={{
+                        padding: 12,
+                        borderRadius: 30,
+                        backgroundColor: focused ? Colors.tintColor : Colors.grey
+                    }}>
+                        <FontAwesome name='user-o' size={18} color={color} />
+                    </View>
+                )
+            }} />
+        </Tabs>
+    )
 }
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NetflixIcon } from '@/constants/icons'
+
+export default function Page() {
+    return (
+        <View>
+            <Text>Page</Text>
+        </View>
+    )
+}
+
